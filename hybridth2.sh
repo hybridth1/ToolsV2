b87f4d2a97108510c83794aee93bbeab4b9a10b3
hybridth1 <47426946+hybridth1@users.noreply.github.com>
2019-02-09 23:05:35 +0700

    Add files via upload

---
+++ hybridth2.sh
@@ -0,0 +1,334 @@
+#!/system/xbin/bash
+clear    
+blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+sleep 1
+echo $red
+echo ""
+echo $blue "Created by :hybridth"
+echo $blue "Team :SPP"
+echo $blue "contact my:bridthh@gmail.com" 
+echo $blue "Enjoy"
+echo
+echo "------------------------------->"
+echo $white "1.Defacer"
+sleep 1					
+echo "------------------------------->"
+echo $white "2.Darkfly-Tools"
+sleep 1
+echo "------------------------------->"
+echo $white "3.Admin-Panel-Finder"
+sleep 1					
+echo "------------------------------->"
+echo $white "4.List Dork"
+sleep 1					
+echo "------------------------------->"
+echo $white "5.Tools-x"
+sleep 1					
+echo "------------------------------->"
+echo $white "6.XAttacker"
+sleep 1					
+echo "------------------------------->"
+echo $white "7.Phising"
+sleep 1 				
+echo "------------------------------->"
+echo $white "8.LTEDDOS"
+sleep 1 				
+echo "------------------------------->"
+echo $white "9.Malicious"
+sleep 1 				
+echo "------------------------------->"
+echo $white "10.IP_Tracker"
+sleep 1 				
+echo "------------------------------->"
+echo $white "11.Santet-Online"
+sleep 1
+echo "------------------------------->"
+echo $white "12.LITESPAM"
+sleep 1
+echo "------------------------------->"
+echo $white "13.Hammer"
+sleep 1
+echo "------------------------------->"
+echo $white "14.MBF"
+sleep 1
+echo "------------------------------->"
+echo $white "15.Hash"
+sleep 1
+echo "------------------------------->"
+echo $white "16.A-Rat"
+sleep 1
+echo "------------------------------->"
+echo $white "17.Metasploit"
+sleep 1
+echo "------------------------------->"
+echo $white "18.Ngrok"
+sleep 1
+echo "------------------------------->"
+echo $white "19.Cek Ip Web"
+sleep 1
+echo "------------------------------->"
+echo $white "20.Ko-Dork"
+sleep 1
+echo "------------------------------->"
+echo $white "21.Sqlmap"
+sleep 1
+echo "------------------------------->"
+echo $white "22.LITESCRIPT"
+sleep 1
+echo "------------------------------->"
+echo $white "23.Webdav"
+sleep 1
+echo "------------------------------->"
+echo $white "24.Nuyul Kubik"
+sleep 1
+echo "------------------------------->"
+echo $white "25.Osif"
+sleep 1
+echo "------------------------------->"
+echo $white "0.Out"
+sleep 1
+echo $blue "------------------------------->"
+echo "\033[32;1m"
+read -p "Mr.Hybridth-->" bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+figlet "Mr.Hybridth" | lolcat
+git clone https://github.com/21D4N404/Defacer
+mv Defaver $HOME
+cd $HOME/Defacer
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+figlet "Mr.Hybridth" | lolcat
+git clone https://github.com/Ranginang67/DarkFly-Tool
+mv DarkFly-Tool $HOME
+cd $HOME/DarkFly-Tool
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+figlet "Mr.Hybridth" | lolcat
+git clone https://github.com/the-robot/admin-finder
+mv admin-finder $HOME
+cd $HOME/admin-finder
+fi
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+figlet "Mr.Hybridth" | lolcat
+git clone https://github.com/hybridth1/Dork_List/
+mv Dork_List $HOME
+cd $HOME/Dork_List
+fi
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+clear
+figlet "Mr.Hybridth"
+git clone https://github.com/Rajkumrdusad/Tool-X
+mv Tool-X $HOME
+cd $HOME/Tool-X
+fi
+
+if [ $bro = 6 ] || [ $bro = 6 ]
+then
+clear
+figlet "Mr.Hybridth"
+git clone https://github.com/Moham3dRiahi/XAttacker
+mv XAttacker $HOME
+cd $HOME/XAttacker
+fi
+
+if [ $bro = 7 ] || [ $bro = 7 ]
+then
+clear
+figlet "Mr.Hybridth"
+git clone https://github.com/Senitopeng/Mancing
+mv Mancing $HOME
+cd $HOME/Mancing
+fi
+
+if [ $bro = 8 ] || [ $bro = 8 ]
+then
+clear
+figlet "Mr.Hybridth"
+git clone https://github.com/4L13199/LITEDDOS
+mv LITEDDOS $HOME
+cd $HOME/LITEDDOS
+fi
+
+if [ $bro = 9 ] || [ $bro = 9 ]
+then
+clear
+figlet "Mr.Hybridth"
+git clone https://github.com/Hider5/Malicious
+mv Malicious $HOME
+cd $HOME/Malicious
+fi
+
+if [ $bro = 10 ] || [ $bro = 10 ]
+then
+clear
+figlet "Mr.Hybridth"
+git clone https://github.com/Rajkumrdusad/IP-Tracer
+mv IP-Tracer $HOME
+cd $HOME/IP-Tracer
+fi
+
+if [ $bro = 11 ] || [ $bro = 11 ]
+then
+clear
+figlet "Mr.Hybridth"
+git clone https://github.com/Gameye98/santet-online
+mv santet-online $HOME
+cd $HOME/santet-online
+fi
+
+if [ $bro = 12 ] || [ $bro = 12 ]
+then
+clear
+figlet "Mr.Hybridth"
+git clone https://github.com/4L13199/LITESPAM
+mv LITESPAM $HOME
+cd $HOME/LITESPAM
+fi
+
+if [ $bro = 13 ] || [ $bro = 13 ]
+then
+clear
+figlet "Mr.Hybridth"
+git clone https://github.com/cyweb/hammer
+mv hammer $HOME
+cd $HOME/hammer
+fi
+
+if [ $bro = 14 ] || [ $bro = 14 ]
+then
+clear
+figlet "Mr.Hybridth"
+git clone https://github.com/YukersCreew/mbf
+mv mbf $HOME
+cd $HOME/mbf
+fi
+
+if [ $bro = 15 ] || [ $bro = 15 ]
+then
+clear
+figlet "Mr.Hybridth"
+git clone https://github.com/Kwntwl/hash
+mv hash $HOME
+mv $HOME/hash
+fi
+
+if [ $bro = 16 ] || [ $bro = 16 ]
+then
+clear
+figlet "Mr.Hybridth"
+git clone https://github.com/Xi4u7/A-Rat
+mv A-Rat $HOME
+cd $HOME/A-Rat
+fi
+
+if [ $bro = 17 ] || [ $bro = 17 ]
+then
+clear
+figlet "Mr.Hybridth"
+git clone https://github.com/rapid7/metasploit-framework
+mv metasploit-framework $HOME
+cd $HOME/metasploit-framework
+fi
+
+if [ $bro = 18 ] || [ $bro = 18 ]
+then
+clear
+figlet "Mr.Hybridth"
+curl -S https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip --output ngrok-stable-linux-arm.zip
+mv ngrok-stable-linux-arm.zip $HOME
+cd $HOME/ngrokh-stable-linux-arm.zip
+fi
+
+if [ $bro = 19 ] || [ $bro = 19 ]
+then
+clear
+figlet "Mr.Hybridth"
+echo $purple "ping namasite_tanpa_http.com"
+fi
+
+if [ $bro = 20 ] || [ $bro = 20 ]
+then
+clear
+figlet "Mr.Hybridth"
+git clone https://github.com/CiKu370/ko-dork
+mv ko-dork $HOME
+cd $HOME/ko-dork
+fi
+
+if [ $bro = 21 ] || [ $bro = 21 ]
+then
+clear
+figlet "Mr.Hybridth"
+git clone https://github.com/sqlmapproject/sqlmap
+mv sqlmap $HOME
+cd $HOME/sqlmap
+fi
+
+if [ $bro = 22 ] || [ $bro = 22 ]
+then
+clear
+figlet "Mr.Hybridth"
+git clone https://github.com/4L13199/LITESCRIPT
+mv LITESCRIPT $HOME
+cd $HOME/LITESCRIPT
+fi
+
+if [ $bro = 23 ] || [ $bro = 23 ]
+then
+clear
+figlet "Mr.Hybridth"
+git clone https://github.com/NoiseCrusT/webdav
+mv webdav $HOME
+cd $HOME/webdav
+fi
+
+if [ $bro = 24 ] || [ $bro = 24 ]
+then
+clear
+figlet "MrHybridth"
+git clone https://github.com/radenvodka/kubik-bot
+mv kubik-bot
+cd $HOME/kubik-bot
+fi
+
+if [ $bro = 25 ] || [ $bro = 25 ]
+then
+clear
+figlet "Mr.Hybridth"
+git clone https://github.com/CiKu370/OSIF
+mv OSIF $HOME
+cd $HOME/OSIF
+fi
+
+if [ $bro = 0 ] || [ $bro = 0 ]
+then
+echo "Jangan Menyerah Sebelum Berhasil"
+sleep 2
+echo "Karna Semua Perjuangan Tidak Ada Yang Sia-Sia"
+sleep 2
+echo "Teruslah Berusaha Sampai Kau Mempunyai Hasil"
+sleep 2
+echo "Good Luck In Your Day"
+sleep 2
+exit
+fi
